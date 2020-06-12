83dd446fbae4493b093a76617e81af103cff7a5f
Massqerix <62904344+Massqerix@users.noreply.github.com>
2020-06-12 22:49:29 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+const Discord = require('discord.js');
+const { prefix, token } = require('./config.json');
+const { readdirSync } = require('fs');
+const bot = new Discord.Client();
+const delay = (msec) => new Promise((response) => setTimeout(response, msec));
+const name = "John";
+
+bot.on('message', async msg => {
+    if(msg.author.bot || !msg.content.startsWith(prefix)) return;
+    const args = msg.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    try {
+        const commandFile = require(`./commands/${command}.js`);
+        commandFile.run(bot, msg, args, name);
+    } catch(e) {
+        return;
+    }
+})
+
+bot.on('ready', async () => {
+    const files = readdirSync('./commands/');
+    files.forEach(file => {
+        if(file.endsWith('.js')){
+            console.log(`Loaded ${file}`);
+        }
+    })
+    console.log(`Loaded ${files.length} commands.`)
+    await delay(2000);
+    console.log(`Everything is loaded and the bot works with a latency of ${bot.ws.ping}ms.`)
+})
+
+bot.login(token);
